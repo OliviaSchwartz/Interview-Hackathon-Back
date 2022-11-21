56d43d97ad1833c78054dfdcc261858b506823ec
Amaya Lucas <amayajlucas@gmail.com>
2022-11-21 12:35:01 -0500

    added some student and course controllers and routes

--- routes/CourseRouter.js
+++ routes/CourseRouter.js
@@ -2,5 +2,8 @@ const Router = require('express').Router()
 const controller = require('../controllers/CourseController')
 
 Router.get('/', controller.getAllCourses)
+Router.get('/:course_id', controller.getCourseById)
+Router.post('/addCourse', controller.addCourse)
+Router.post('/:course_id/addStudent', controller.addStudentToCourse)
 
 module.exports = Router
