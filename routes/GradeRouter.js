ca99140f7a4eae0989a83e0180ef764bd7ae22c3
Amaya Lucas <amayajlucas@gmail.com>
2022-11-22 11:38:22 -0500

    added protections to routes

--- routes/GradeRouter.js
+++ routes/GradeRouter.js
@@ -1,7 +1,18 @@
 const Router = require('express').Router()
 const controller = require('../controllers/GradeController')
+const middleware = require('../middleware')
 
-Router.put('/:course_id/:student_id', controller.updateGrade)
-Router.get('/:course_id/:student_id', controller.getGrade)
+Router.put(
+  '/:course_id/:student_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.updateGrade
+)
+Router.get(
+  '/:course_id/:student_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.getGrade
+)
 
 module.exports = Router
--- routes/StudentRouter.js
+++ routes/StudentRouter.js
@@ -1,9 +1,25 @@
 const Router = require('express').Router()
 const controller = require('../controllers/StudentController')
+const middleware = require('../middleware')
 
 Router.get('/', controller.getAllStudents)
-Router.get('/:student_id', controller.getStudentById)
-Router.delete('/:student_id', controller.deleteStudent)
-Router.put('/gpa/:student_id', controller.updateGpa)
+Router.get(
+  '/:student_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.getStudentById
+)
+Router.delete(
+  '/:student_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.deleteStudent
+)
+Router.put(
+  '/gpa/:student_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.updateGpa
+)
 
 module.exports = Router
