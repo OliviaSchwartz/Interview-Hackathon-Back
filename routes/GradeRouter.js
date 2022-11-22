a1b32b90c6cdb007e4744476dfd53673e2d96c8d
Amaya Lucas <amayajlucas@gmail.com>
2022-11-21 21:02:41 -0500

    getGrade controller and route

--- routes/GradeRouter.js
+++ routes/GradeRouter.js
@@ -2,5 +2,6 @@ const Router = require('express').Router()
 const controller = require('../controllers/GradeController')
 
 Router.put('/:course_id/:student_id', controller.updateGrade)
+Router.get('/:course_id/:student_id', controller.getGrade)
 
 module.exports = Router
